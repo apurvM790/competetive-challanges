9c9489a90011dee2660ee0a7380d6aee4e680703
apurvM790 <97803586+apurvM790@users.noreply.github.com>
2022-11-09 22:46:05 +0530

    Create Online_Stock_Span.cpp

---
+++ LEET-CODE/C++/Online_Stock_Span.cpp
@@ -0,0 +1,28 @@
+class StockSpanner {
+public:
+    vector<int> prev;
+    StockSpanner() {
+        
+        //StockSpanner* obj = new StockSpanner();
+        
+    }
+    
+    stack<pair<int,int>> st;
+    int next(int price) {
+        int count=0;
+        while(!st.empty() and st.top().first<=price)
+        {
+            count+=st.top().second;
+            st.pop();
+        }
+        count++;
+        st.push({price,count});
+        return count;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
